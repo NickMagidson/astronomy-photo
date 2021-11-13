3824e2e2b02bcbaa873ac35886f2889072952c4f
Nick <nickmagi24@gmail.com>
2021-11-13 09:41:39 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//V3So8Qu3NHWIE20l3VCTlXhyZscIKTZK7W1vhJS8
+
+// https://api.nasa.gov/planetary/apod?api_key=V3So8Qu3NHWIE20l3VCTlXhyZscIKTZK7W1vhJS8
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      photo: []
+    }
+  };
+
+
+  componentDidMount() {
+    let url = 'https://api.nasa.gov/planetary/apod?api_key=V3So8Qu3NHWIE20l3VCTlXhyZscIKTZK7W1vhJS8';
+    fetch(url) 
+      .then(response => response.json())
+      .then(content => {
+        this.setState({
+          photo: content.url
+        })
+
+      })
+    console.log(url)
+  }
+
+
+  render() {
+    return(
+      <div className="app">
+       <img src={this.state.photo} />
+      </div>
+    )
+  }
+
 }
 
 export default App;
