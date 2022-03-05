a781be5dd9bbe96d7ea8bc405ba1e60a79948c71
Nick <nickmagi24@gmail.com>
2022-03-05 12:15:13 -0500

    Mobile style edits

--- src/Photo.js
+++ src/Photo.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './style2.css'
+import video from './video 1.mp4';
 
 const API_KEY = 'V3So8Qu3NHWIE20l3VCTlXhyZscIKTZK7W1vhJS8';
 const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
@@ -26,8 +27,11 @@ class Photo extends Component {
   render() {
     return(
       <div className="photo-container">
-      <a href={this.state.photo} target="_blank"><img src={this.state.photo} className="photo" alt="Astronomy Picture of the" /></a>
-    </div>
+        <a href={this.state.photo} target="_blank"><img src={this.state.photo} className="photo" alt="Astronomy Picture of the" /></a>
+          {/* <video className="bgVideo" autoPlay loop muted id="video">
+            <source src={video} type='video/mp4'></source>
+          </video> */}
+      </div>
     )
   }
 }
