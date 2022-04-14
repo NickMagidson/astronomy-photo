44a4215ec639da05a43dcc92ba099381138f903c
Nick Magidson <Nickmagi24@gmail.com>
2022-04-14 19:15:32 -0400

    Photo component refactor from class to hooks

--- src/Photo.js
+++ src/Photo.js
@@ -1,39 +1,24 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import './style2.css'
-import video from './video 1.mp4';
 
 const API_KEY = 'V3So8Qu3NHWIE20l3VCTlXhyZscIKTZK7W1vhJS8';
 const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
 
-class Photo extends Component {
-  constructor(props) {
-    super(props)
+function Photo() {
+  let [photo, setPhoto] = useState([]);
 
-    this.state = {
-        photo: []
-      }
-  }
-
-  componentDidMount() {
-  fetch(API_URL) 
+  useEffect(() => {
+    fetch(API_URL)
     .then(response => response.json())
-    .then(content => {
-      this.setState({
-        photo: content.hdurl
-      })
-    })
-  }
+    .then(content => setPhoto(content.hdurl))
+  }, [])
 
-  render() {
-    return(
-      <div className="photo-container">
-        <a href={this.state.photo} target="_blank"><img src={this.state.photo} className="photo" alt="Astronomy Picture of the" /></a>
-          {/* <video className="bgVideo" autoPlay loop muted id="video">
-            <source src={video} type='video/mp4'></source>
-          </video> */}
-      </div>
-    )
-  }
+
+  return (
+    <div className="photo-container">
+      <a href={photo} target="_blank"><img src={photo} className="photo" alt="Astronomy Picture of the" /></a>
+    </div>
+  )
 }
 
 export default Photo;
