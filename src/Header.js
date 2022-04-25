68bac4ebf95484f110a174a9ac4a5ae07be6e025
Nick Magidson <Nickmagi24@gmail.com>
2022-04-25 10:13:45 -0400

    Datepicker z-index fix

--- src/Header.js
+++ src/Header.js
@@ -4,18 +4,15 @@ import Photo from "./Photo";
 
 import moment from 'moment';
 
+const API_KEY = 'V3So8Qu3NHWIE20l3VCTlXhyZscIKTZK7W1vhJS8';
+const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
 // Edits on this component are still in progress
 class Header extends Component {
 	state = {
 		date: moment(),
 		photo: <Photo />
 	}
-
-	getPhoto = date => {
-    fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=DEMO_KEY`)
-      .then(response => response.json())
-      .then(photoData => this.setState({ photo: photoData }));
-  };
 
 	formatDate = moment => {
 		let year = moment.year;
@@ -24,19 +21,26 @@ class Header extends Component {
 		return `${year}-${month}-${day}`;
 	}
 
+
 	changeDate = dateFromInput => {
 		this.setState({ date: dateFromInput });
 		this.getPhoto(this.formatDate(dateFromInput)); // getPhoto should be linked to a block of code that fetches the api photo
 	}
 
+	getPhoto = date => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(photoData => this.setState({ photo: photoData }));
+	}
+
 	render() {
-		return(
+		return (
 			<div className="header">
 				<h2 className="title"> Astronomy Photo of the Day</h2>
 				<DateInput changeDate={this.changeDate} />
 			</div>
 		)
-	}
+	};
 
 }
 
