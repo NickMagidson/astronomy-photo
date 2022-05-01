38d4ed00f7a00cb443b9ad347cae9bdf2c3477df
Nick Magidson <Nickmagi24@gmail.com>
2022-05-01 16:32:40 -0400

    Comments

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import Photo from './Photo';
 import './style2.css';
 import video from './video 1.mp4';
 
+
+// State and stuff goes here...
+
+// 1. Header JSX in lieu of component in App(). Also <DateInput />
+// 2. Main will have <video> & <Info />
+// 3. Make sure styling and fetching are properlly working
+
 function App() {
   return (
     <div className="app">
